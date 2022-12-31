fa259c5858bede61611dbc0e54e0e1bdd84f2643
Robin Grönberg <robingronberg@gmail.com>
2022-12-31 01:24:55 +0100

    Stub for main proc_macro.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn main(_attr: TokenStream, _item: TokenStream) -> TokenStream {
+    // dbg!(&_attr);
+    // dbg!(&_item);
+    _item
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
