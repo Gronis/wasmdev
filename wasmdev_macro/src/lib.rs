7383bbb8aea18b338c65008fe76abea1cae4ab8b
Robin Grönberg <robingronberg@gmail.com>
2023-01-27 02:12:07 +0100

    More cleanup.

--- wasmdev_macro/src/lib.rs
+++ wasmdev_macro/src/lib.rs
@@ -5,49 +5,35 @@ use quote::quote;
 #[proc_macro_attribute]
 pub fn main(_attr: TokenStream, main_fn: TokenStream) -> TokenStream {
 
-    // We need to convert main function to better work with it.
-    let main_fn_wasm: TokenStream2 = main_fn.into();
-    
-    // Extract the body of the main function.
-    let Some(main_fn_wasm_body) = make_function_body_clone(&main_fn_wasm) else { 
-        panic!("function does not have a body.")
-    };
+    let wasm_main_fn:   TokenStream2 = main_fn.into();
+    let server_main_fn: TokenStream2 = make_server_main_fn(&wasm_main_fn);
 
-    // Make the code for the http web server:
-    let main_fn_not_wasm: TokenStream2 = make_main_fn_not_wasm(main_fn_wasm_body);
-
-    // dbg!(&_attr);
-    // dbg!(&main_fn_wasm);
-    // dbg!(&main_fn_not_wasm);
-
-    // Output the rust code for wasm and server main functions:
     quote! {
         #[cfg(not(target_family = "wasm"))]
-        #main_fn_not_wasm
+        #server_main_fn
         #[cfg(target_family = "wasm")]
-        #main_fn_wasm
+        #wasm_main_fn
     }.into()
 }
 
-fn make_function_body_clone(function: &TokenStream2) -> Option<TokenTree> {
+fn clone_fn_body(function: &TokenStream2) -> Option<TokenTree> {
     function.clone().into_iter().find_map(|token| {
         let TokenTree::Group(group) = &token else { return None };
         if group.delimiter() == Delimiter::Brace { Some(token) } else { None }
     })
 }
 
-fn make_main_fn_not_wasm(main_fn_wasm_body: TokenTree) -> TokenStream2 {
+fn make_server_main_fn(wasm_main_fn: &TokenStream2) -> TokenStream2 {
+    let Some(wasm_body) = clone_fn_body(&wasm_main_fn) else { panic!("function does not have a body.") };
     let _script = format!("<script>{}</script>", include_str!("index.js"));
     quote!{
         fn main() {
-            // non-wasm only imports:
             use std::net::TcpListener;
             use wasmdev::Server;
 
             // Make sure rust analyzer (code-completion) analyze the wasm code:
-            if false #main_fn_wasm_body
-            
-            // Setup and configure http server:
+            if false #wasm_body
+
             let tcp_socket = TcpListener::bind("127.0.0.1:8123").expect("Unable to bind tcp port 8123");
             let mut server = Server::new(tcp_socket);
 
@@ -59,12 +45,9 @@ fn make_main_fn_not_wasm(main_fn_wasm_body: TokenTree) -> TokenStream2 {
             // - add hot reload to wasm code
             // - figure out a way to run tests in browser, but show everyting in cli
 
-            // Tell user how to run the app:
             println!("\x1b[1m\x1b[92m            \x1b[0m ┏\x1b[0m━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m┓");
             println!("\x1b[1m\x1b[92m     Serving\x1b[0m ┃\x1b[1m http://127.0.0.1:8123 \x1b[0m┃ <- Click to open your app! ");
             println!("\x1b[1m\x1b[92m            \x1b[0m ┗\x1b[0m━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m┛");
-
-            // Start the http server:
             server.listen().expect("Unable to handle incomming connection");
         }
     }
