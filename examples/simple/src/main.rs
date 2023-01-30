0e32f28773ef2f4c681a6de8c5aa85931e51ba80
Robin Grönberg <robingronberg@gmail.com>
2023-01-30 03:28:35 +0100

    Make simple example more simple. Re-add websocket on client side.

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,5 +1,22 @@
+
 #[wasmdev::main]
 fn main() {
     console_error_panic_hook::set_once();
-    simple::app();
+
+    // Grab document body
+    let window = web_sys::window()
+        .expect("no global `window` exists");
+    let document = window.document()
+        .expect("should have a document on window");
+    let body = document.body()
+        .expect("document should have a body");
+
+    // Create Hello World paragraph
+    let val = document.create_element("p")
+        .expect("Unable to create element");
+    val.set_text_content(Some("Hello Rust!"));
+
+    // Append text to document body:
+    body.append_child(&val)
+        .expect("Unable to append element");
 }
--- wasmdev_macro/src/index.js
+++ wasmdev_macro/src/index.js
@@ -1,2 +1,11 @@
 import init from '/index.js';
 init("/index.wasm");
+
+let url = "ws://" + window.location.host;
+let ws = new WebSocket(url);
+ws.onopen = () => {
+    ws.send("Hello WebSocket");
+};
+ws.onmessage = msg => {
+    console.log("WebSocket message:", msg);
+}
