875d9e8bc927a9225b8967592d7462120e530e2e
Robin Grönberg <robingronberg@gmail.com>
2023-02-28 16:52:15 +0100

    Add ability to provide custom address. Start using compile_error macro rather than panic.

--- wasmdev_macro/src/lib.rs
+++ wasmdev_macro/src/lib.rs
@@ -1,15 +1,27 @@
 use proc_macro::TokenStream;
-use proc_macro2::{TokenStream as TokenStream2, TokenTree};
+use proc_macro2::{TokenStream as TokenStream2, TokenTree, Literal, Punct, Spacing, Ident, Group, Delimiter, Span};
 use quote::quote;
 use std::env;
 
 mod util;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct Config {
     port: u16,
-    path: Option<String>,
+    path: String,
+    addr: String,
 }
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            port: 8080,
+            path: "src".into(),
+            addr: "127.0.0.1".into(),
+        }
+    }
+}
+
 ///
 /// Turns the main function for non-`wasm` targets into a hot-reload development web-server.
 /// 
@@ -58,8 +70,12 @@ struct Config {
 #[proc_macro_attribute]
 pub fn main(attrs: TokenStream, main_fn: TokenStream) -> TokenStream {
 
+    let config = match parse_config_attrs(attrs.into()) { 
+        Ok(config) => config, 
+        Err(ts) => return ts.into()
+    };
+
     let wasm_fn: TokenStream2 = main_fn.into();
-    let config                = parse_config_attrs(attrs.into());
     let wasm_main_fn          = make_wasm_main_fn(&wasm_fn);
     let server_main_fn        = make_server_main_fn(&wasm_fn, config);
 
@@ -71,46 +87,113 @@ pub fn main(attrs: TokenStream, main_fn: TokenStream) -> TokenStream {
     }.into()
 }
 
-fn parse_config_attrs(attrs: TokenStream2) -> Config {
+fn emit_compilation_error(msg: &str, span: &Span) -> TokenStream2 {
+    let span = span.clone();
+    TokenStream2::from_iter(vec![
+        TokenTree::Ident(Ident::new("compile_error", span)),
+        TokenTree::Punct({
+            let mut punct = Punct::new('!', Spacing::Alone);
+            punct.set_span(span);
+            punct
+        }),
+        TokenTree::Group({
+            let mut group = Group::new(Delimiter::Brace, {
+                TokenStream2::from_iter(vec![TokenTree::Literal({
+                    let mut string = Literal::string(msg);
+                    string.set_span(span);
+                    string
+                })])
+            });
+            group.set_span(span);
+            group
+        }),
+    ])
+}
+
+macro_rules! compiler_error { 
+    ($i:ident, $($args:tt)*) => { 
+        {
+            let span = ($i).span();
+            Err(emit_compilation_error(&format!($($args)*), &span))
+        }
+    };
+    ( $($args:tt)*) => { 
+        {
+            let span = Span::call_site();
+            Err(emit_compilation_error(&format!($($args)*), &span))
+        }
+    };
+}
+// Err(emit_compilation_error(&format!(msg), span)
+
+fn parse_config_attrs(attrs: TokenStream2) -> Result<Config, TokenStream2> {
     let mut it = attrs.into_iter();
     let mut port: u16 = 8080;
     let mut path = None;
+    let mut addr = None;
+
+    struct ParseError;
+
+    let literal_to_string = |value: &Literal| -> Result<String, ParseError> {
+        let value = value.to_string();
+        if value.as_bytes()[0] != ('"' as u8) || value.as_bytes()[value.as_bytes().len() - 1] != ('"' as u8) { 
+            return Err(ParseError);
+        }
+        Ok(value[1..value.len() - 1].to_string())
+    };
 
     loop {
+        
         let Some(TokenTree::Ident(ident)) = it.next() else { break };
-        let Some(TokenTree::Punct(punct)) = it.next() else { panic!("Incomplete attribute list, expected '=' or ':' after '{ident}'") };
-        let Some(TokenTree::Literal(value)) = it.next() else { panic!("Incomplete attribute list, expected value after '{punct}'") };
-
+        let Some(TokenTree::Punct(punct)) = it.next() else { 
+            return compiler_error!(ident, "Incomplete attribute list, expected '=' or ':' after '{ident}'")
+        };
+        
         match punct.to_string().as_str() {
             ":" => (),
             "=" => (),
-            _ => panic!("Must use ':' or '=' to assign a value to '{ident}'"),
+            _ => return compiler_error!(punct, "Unexpected character '{punct}'. Expected ':' or '='"),
+        };
+        
+        let Some(TokenTree::Literal(value)) = it.next() else { 
+            return compiler_error!(punct, "Incomplete attribute list, expected value after '{punct}'")
         };
 
         match ident.to_string().as_str() {
             "port" => { 
-                let Some(p) = value.to_string().parse().ok() else { 
-                    panic!("Unable to parse port, {value} is not a u16")
+                let Ok(p) = value.to_string().parse() else { 
+                    return compiler_error!(value, "Unable to parse port, {value} is not a u16");
                 };
                 port = p;
             },
-            "path" => {
-                let value = value.to_string();
-                if value.as_bytes()[0] != ('"' as u8) || value.as_bytes()[value.as_bytes().len() - 1] != ('"' as u8) { 
-                    panic!("Unable to set path, {value} is not a &str literal")
-                }
-                path = Some(value[1..value.len() - 1].to_string())
+            "path" => { 
+                let Ok(p) = literal_to_string(&value) else {
+                    return compiler_error!(value, "Unable to parse path, {value} is not a `&str`");
+                };
+                path = Some(p);
             },
-            ident  => panic!("Unknown attribute: {ident}, Tip: possible attributes in {:?}", Config::default()),
+            "addr" => { 
+                let Ok(a) = literal_to_string(&value) else {
+                    return compiler_error!(value, "Unable to parse addr, {value} is not a `&str`");                    
+                };
+                addr = Some(a);
+            },
+            i  => { 
+                return compiler_error!(ident, "Unknown attribute: '{i}', Tip: possible attributes in {:?}", Config::default());
+            },
         }
 
         match it.next() {
             Some(TokenTree::Punct(punct)) if punct.to_string() == "," => (),
             None => (),
-            Some(tt) => panic!("Unrecognised character '{tt}', Tip: use ',' to separate attributes."),
+            Some(tt) => return compiler_error!(tt, "Unexpected character '{tt}', Tip: use ',' to separate attributes."),
         }
     };
-    Config { port, path }
+    Ok(Config { 
+        port, 
+        path: path.unwrap_or(Config::default().path), 
+        addr: addr.unwrap_or(Config::default().addr),
+    })
 }
 
 fn get_fn_name(func: &TokenStream2) -> Option<TokenTree> {
@@ -143,7 +226,8 @@ fn make_server_main_fn(wasm_main_fn: &TokenStream2, config: Config) -> TokenStre
         .expect("Cargo did not set env var: CARGO_PKG_NAME");
 
     let server_port      = config.port;
-    let server_path      = config.path.unwrap_or("src".to_string());
+    let server_path      = config.path;
+    let server_addr      = config.addr;
     let is_release       = !cfg!(debug_assertions);
     let index_js         = include_str!("index.js");
     let index_html       = include_str!("index.html");
@@ -159,6 +243,13 @@ fn make_server_main_fn(wasm_main_fn: &TokenStream2, config: Config) -> TokenStre
     let proj_server_path = format!("{proj_dir}/{server_path}");
     let proj_src_path    = format!("{proj_dir}/src");
     let build_wasm_now   = env::var("CARGO_WASMDEV").ok().is_none() && is_release;
+
+    // Check that server path for static assets exists:
+    let _ = std::fs::metadata(&proj_server_path)
+        .expect(&format!("Error: Unable to read directory: {}", &proj_server_path));
+    // Check that provided ip address is an ip address:
+    let _ = server_addr.parse::<std::net::IpAddr>()
+        .expect(&format!("Error: {} is not a valid ipv4 or ipv6 address", &server_addr));
 
     let invalidate_static_asset_cache = if build_wasm_now {
         use std::fs;
@@ -264,6 +355,7 @@ fn make_server_main_fn(wasm_main_fn: &TokenStream2, config: Config) -> TokenStre
                 let proj_html_path   = #proj_html_path;
                 let proj_server_path = #proj_server_path;
                 let proj_src_path    = #proj_src_path;
+                let server_addr      = #server_addr;
                 let server_port      = #server_port;
 
                 let server = Server::new();
@@ -381,11 +473,17 @@ fn make_server_main_fn(wasm_main_fn: &TokenStream2, config: Config) -> TokenStre
                         // Providing a custom index.html is optional, so open watcher is allowed to fail silently here.
                 ))};
                 
-                println!("\x1b[1m\x1b[92m            \x1b[0m ┏\x1b[0m━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m┓");
-                println!("\x1b[1m\x1b[92m     Serving\x1b[0m ┃\x1b[1m  http://127.0.0.1:{   } \x1b[0m┃ <= Click to open your app! ", format!("{: <5}", server_port));
-                println!("\x1b[1m\x1b[92m            \x1b[0m ┗\x1b[0m━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m┛");
+                let addr_char_count = server_addr.chars().into_iter().count();
+                print!("\x1b[1m\x1b[92m            \x1b[0m ┏\x1b[0m━━━━━━━━━");
+                for _ in 0..addr_char_count { print!("━") };
+                println!("━━━━━━━\x1b[0m┓");
+                println!("\x1b[1m\x1b[92m     Serving\x1b[0m ┃\x1b[1m  http://{}:{} \x1b[0m┃ <= Click to open your app! ", server_addr, format!("{: <5}", server_port));
+                print!("\x1b[1m\x1b[92m            \x1b[0m ┗\x1b[0m━━━━━━━━━");
+                for _ in 0..addr_char_count { print!("━") };
+                println!("━━━━━━━\x1b[0m┛");
                 
-                let addr = format!("127.0.0.1:{}", server_port);
+                // let addr = format!("127.0.0.1:{}", server_port);
+                let addr = format!("{}:{}", server_addr, server_port);
                 let Some(tcp_socket) = TcpListener::bind(addr).ok() else { 
                     panic!("Unable to bind tcp port: {}", server_port)
                 };
