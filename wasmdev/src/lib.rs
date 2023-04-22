3714364d13b06c39a5c9ffffa47976b4fa0a303a
Robin Grönberg <robingronberg@gmail.com>
2023-04-22 14:57:14 +0200

    Add nightly feature which produces smaller bin size.

--- wasmdev/src/lib.rs
+++ wasmdev/src/lib.rs
@@ -3,12 +3,12 @@ pub use wasmdev_macro::main;
 #[cfg(not(target_family = "wasm"))]
 pub use wasmdev_server::*;
 
-#[cfg(all(target_family = "wasm", feature = "panic_hook"))]
+#[cfg(all(target_family = "wasm", feature = "panic_hook", debug_assertions))]
 #[inline]
 pub fn if_enabled_setup_panic_hook_once() {
     console_error_panic_hook::set_once();
 }
 
-#[cfg(not(all(target_family = "wasm", feature = "panic_hook")))]
+#[cfg(not(all(target_family = "wasm", feature = "panic_hook", debug_assertions)))]
 #[inline]
 pub fn if_enabled_setup_panic_hook_once() {}
--- wasmdev_core/src/code.rs
+++ wasmdev_core/src/code.rs
@@ -6,24 +6,49 @@ use wasm_bindgen_cli_support::Bindgen;
 pub fn build_wasm(input_path: impl AsRef<Path>, is_release: bool, target_dir: impl AsRef<Path>) -> Option<()> {
     let target_dir = target_dir.as_ref().to_str()?;
     let mut args = vec![
+        "build",
         "--target", "wasm32-unknown-unknown",
         "--target-dir", target_dir,
         "--color", "always",
     ];
-    if is_release { args.push("--release") };
-    let args = args; // Remove mutability;
+    if is_release { 
+        // Minimize bundle size by making the panic handler exit silently.
+        // See: https://github.com/rust-lang/rust/issues/54981
+        // Only enabled if nightly toolchain is used with nightly feature. 
+        // Enable later for stable release
+        if cfg!(feature = "nightly") {
+            args.push("-Z");
+            args.push("build-std=std,panic_abort");
+            args.push("-Z");
+            args.push("build-std-features=panic_immediate_abort");
+        }
+        // FIXME: Check if lto and opt-level is set in Cargo.toml, if not, use these defaults.
+        // Because programmers are lazy, always use these optimized defaults for now.
+        if true {
+            args.push("--config");
+            args.push("profile.release.lto=true");
+            args.push("--config");
+            args.push("profile.release.opt-level='z'");
+            args.push("--config");
+            args.push("profile.release.codegen-units=1");
+            args.push("--config");
+            args.push("profile.release.panic='abort'");
+        }
+        args.push("--release");
+    };
+    let args = args; // Remove mut
     let sh = Shell::new().expect("Unable to create shell");
     {
         // This lets wasmdev::main know if cargo was started from within wasmdev::main
         let _env_guard = sh.push_env("CARGO_WASMDEV", "1");
-        cmd!(sh, "cargo build").args(args).quiet().run().ok()?;
+        cmd!(sh, "cargo").args(args).quiet().run().ok()?;
     }
     let output_path = input_path.as_ref().parent()?;
     Bindgen::new()
         .input_path(&input_path)
         .web(true)
         .map_err(|err| println!("{}", err)).ok()?
-        .demangle(!is_release)
+        .demangle(is_release)
         .debug(!is_release)
         .remove_name_section(is_release)
         .remove_producers_section(is_release)
